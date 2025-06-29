2ca0548df3da5518c8d69a9b3ecabc13427fe505
Kuzhen Larn <kolia01122006@gmail.com>
2025-06-29 17:24:27 +0300

    KuzhenFile

---
+++ script.js
@@ -0,0 +1,10 @@
+function menu(o){
+    if(o == false)
+        {
+            document.getElementById("menu").style.display = "block";
+        }
+    if(o == true)
+        {
+            document.getElementById("menu").style.display = "none";
+        }
+}
