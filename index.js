775f41da99e25fc377f32ccf1c7ec8f3de0bb54c
Kuzhen Larn <kolia01122006@gmail.com>
2025-10-02 15:22:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const WelcomeText = "Kuzhen Project - це Українська команда яка займається розробкою ігор.";
 let isTyping = false;
 let currentText = "";
+let gondo = 0;
 let textIndex = 0;
 const texts = [
   "Kuzhen Project - це Українська команда яка займається розробкою ігор.",
@@ -8,37 +9,11 @@ const texts = [
   "Є можливість додати свій запит на приєднання в команду Кужен Проєкт.",
   "Моніторьте який прогрес в розробці гри та хто саме це робить."
 ];
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { // Виконується, коли весь HTML завантажений і розібраний
     startTypingCycle();
     document.getElementById("start_p").style.fontSize = "25px";
+    document.getElementById("ico_kuzhen_project").style.transitionDuration = "0.4s";
 });
-
-window.addEventListener('scroll', function() {
-    const element = document.getElementById('myElement');
-
-    if (window.scrollY >= 250) {
-        document.getElementById("ico_kuzhen_project").style.top = "-500px";
-    } else {
-        document.getElementById("ico_kuzhen_project").style.top = "40px";
-    }
-
-    if (window.scrollY >= 500) {
-        document.getElementById("bg_block_1").style.top = "100%";
-        document.getElementById("bg_block_1").style.opacity = "1";
-    }
-    if (window.scrollY <= 500) {
-        document.getElementById("bg_block_1").style.top = "1000px";
-        document.getElementById("bg_block_1").style.opacity = "0";
-    }
-    console.debug(window.scrollY);
-});
-
-
-
-
-
-
-
 
 
 
